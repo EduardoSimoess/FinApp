aac1b4afdb1e26fa90510639f1b642693b7fac15
Eduardo Simões <eduardorsimoes97@gmail.com>
2023-05-11 15:50:47 -0300

    stilyng: Estilização da Página inicial

--- frontend/src/Pages/Initial.jsx
+++ frontend/src/Pages/Initial.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 
 function Intial() {
   return (
-    <div>
+    <div className="bg-rose-400
+    h-screen
+    text-5xl
+    font-mono
+    text-white
+    font-bold underline
+    flex
+    flex-col
+    gap-y-11
+    items-center
+    text-center
+    justify-center"
+    >
       <Link to="/new">Adicionar despesa</Link>
       <Link to="/list">Lista de despesas</Link>
     </div>
--- frontend/src/Pages/List.jsx
+++ frontend/src/Pages/List.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 function List() {
-  const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Desenbro'];
+  const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Desembro'];
   return (
-    <div>
+    <div className="bg-rose-400
+    h-screen
+    text-5xl
+    font-mono
+    text-white
+    font-bold underline
+    flex
+    flex-col
+    gap-y-2.5
+    items-center
+    text-center
+    justify-center
+    snap-both
+    my-8"
+    >
       {months.map((month, i) => (
-        <Link to={`/list/${i + 1}`} key={month}>{month}</Link>
+        <Link to={`/list/${i + 1}`} key={month} className="object-contain">{month}</Link>
       ))}
     </div>
   );
